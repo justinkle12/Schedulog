e1abe58ec880abb3bd16da2165dc3ba55b8280fd
Bryant Lam <89410481+bryant-lam@users.noreply.github.com>
2023-08-30 10:58:10 -0700

    swapped android.util.log with timber.log

--- app/src/main/java/com/example/schedulog/MainActivity.kt
+++ app/src/main/java/com/example/schedulog/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.schedulog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -10,10 +9,14 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Timber.i("onCreate Called")
+
         setContentView(R.layout.activity_main)
 
         val writeBtn = findViewById<Button>(R.id.button_testwrite)
@@ -28,12 +31,49 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /** Lifecycle Methods **/
+    /** So far only added logging methods. Can add code to interact with lifecycles.**/
+    override fun onStart() {
+        super.onStart()
+
+        Timber.i("onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart Called")
+    }
+
+
+    /** Functions and methods **/
     fun testWrite(){
         // Write a message to the database
         val database = Firebase.database
         val myRef = database.getReference("Justin")
 
-        myRef.setValue("Hello, World! - Justin")
+        val value = "Hello, World! - Justin"
+        myRef.setValue(value)
+        Timber.i("%s | Writing | %s", myRef, value)
     }
 
     fun testRead(){
@@ -46,11 +86,11 @@ class MainActivity : AppCompatActivity() {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 val value = snapshot.getValue<String>()
-                Log.d("firebase", "Value is: " + value)
+                Timber.i("%s | Reading | %s" , myRef, value)
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Log.w("firebase", "Failed to read value.", error.toException())
+                Timber.i("firebase: Failed to read value", error.toException())
             }
 
         })
