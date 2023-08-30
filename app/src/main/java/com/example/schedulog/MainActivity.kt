43ccc48f7fe6a41cf07a49023a5e211c5bd34734
Bryant Lam <89410481+bryant-lam@users.noreply.github.com>
2023-08-30 09:53:25 -0700

    fixed indentation

--- app/src/main/java/com/example/schedulog/MainActivity.kt
+++ app/src/main/java/com/example/schedulog/MainActivity.kt
@@ -13,46 +13,46 @@ import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            val writeBtn = findViewById<Button>(R.id.button_testwrite)
-            val readBtn = findViewById<Button>(R.id.button_testread)
+        val writeBtn = findViewById<Button>(R.id.button_testwrite)
+        val readBtn = findViewById<Button>(R.id.button_testread)
 
-            writeBtn.setOnClickListener() {
-                testWrite()
+        writeBtn.setOnClickListener() {
+            testWrite()
+        }
+
+        readBtn.setOnClickListener() {
+            testRead()
+        }
+    }
+
+    fun testWrite(){
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("Justin")
+
+        myRef.setValue("Hello, World! - Justin")
+    }
+
+    fun testRead(){
+        val database = Firebase.database
+        val myRef = database.getReference("Justin")
+        // Read from the database
+        myRef.addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<String>()
+                Log.d("firebase", "Value is: " + value)
             }
 
-            readBtn.setOnClickListener() {
-                testRead()
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("firebase", "Failed to read value.", error.toException())
             }
-        }
 
-        fun testWrite(){
-            // Write a message to the database
-            val database = Firebase.database
-            val myRef = database.getReference("Justin")
-
-            myRef.setValue("Hello, World! - Justin")
-        }
-
-        fun testRead(){
-            val database = Firebase.database
-            val myRef = database.getReference("Justin")
-            // Read from the database
-            myRef.addValueEventListener(object: ValueEventListener {
-
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    // This method is called once with the initial value and again
-                    // whenever data at this location is updated.
-                    val value = snapshot.getValue<String>()
-                    Log.d("firebase", "Value is: " + value)
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    Log.w("firebase", "Failed to read value.", error.toException())
-                }
-
-            })
-        }
+        })
+    }
 }
