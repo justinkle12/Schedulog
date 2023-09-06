f279acf2235593426feebe89b44b008e04f034f3
Bryant Lam <89410481+bryant-lam@users.noreply.github.com>
2023-09-06 16:29:39 -0700

    implemented selecting images from an android gallery
    need to check if it is saved to the realtime database correctly and accurately

--- app/src/main/java/com/example/schedulog/MainActivity.kt
+++ app/src/main/java/com/example/schedulog/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.schedulog
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Path
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
+import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -10,20 +18,31 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import timber.log.Timber
+import java.io.ByteArrayOutputStream
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
+    /*variables for selecting images from gallery*/
+    private val pickImage = 100
+    private var imageUri: Uri? = null
+    private var imagePath: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         Timber.i("onCreate Called")
+
+        //setContentView(R.layout.activity_events)
 
         setContentView(R.layout.activity_main)
 
         val writeBtn = findViewById<Button>(R.id.button_testwrite)
         val readBtn = findViewById<Button>(R.id.button_testread)
 
-        writeBtn.setOnClickListener() {
-            testWrite()
+        writeBtn.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
         }
 
         readBtn.setOnClickListener() {
@@ -66,14 +85,49 @@ class MainActivity : AppCompatActivity() {
 
 
     /** Functions and methods **/
-    fun testWrite(){
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            imagePath = imageUri?.let { getRealPathFromURI(it) }
+            Timber.i("imagePath: %s", imagePath)
+
+            testWrite(imagePath)
+        }
+    }
+
+    fun testWrite(selectedImagePath: String?){
         // Write a message to the database
         val database = Firebase.database
-        val myRef = database.getReference("Justin")
+        val myRef = database.getReference("images")
+        // get image from phone
 
-        val value = "Hello, World! - Justin"
+        // convert image to bitmap to bytearray for storing into DB
+        val bm = BitmapFactory.decodeFile(selectedImagePath)
+        val baos = ByteArrayOutputStream()
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos) // bm is the bitmap object
+
+        val b = baos.toByteArray()
+
+        val encodedImage: String = Base64.encodeToString(b, Base64.DEFAULT)
+
+        val value = encodedImage
+
+        // put into DB and show in logcat
         myRef.setValue(value)
         Timber.i("%s | Writing | %s", myRef, value)
+    }
+
+    private fun getRealPathFromURI(contentURI: Uri): String? {
+        val cursor = contentResolver.query(contentURI, null, null, null, null)
+        return if (cursor == null) { // Source is Dropbox or other similar local file path
+            contentURI.path
+        } else {
+            cursor.moveToFirst()
+            val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
+            cursor.getString(idx)
+        }
     }
 
     fun testRead(){
