d67acf931fafca1a6b1005995e1d0bfdd89fd9f2
Bryant Lam <89410481+bryant-lam@users.noreply.github.com>
2023-09-06 17:18:32 -0700

    added testing for images from the DB
    calls onActivityResult to select picture from gallery
    gets Uri and image path
    converts image path to bitmap
    converts to base64 string (for json usage)
    put into DB
    
    to upload image
    get string value from DB
    decode base64 string to bytearray
    decode bytearray to image
    use image

--- app/src/main/java/com/example/schedulog/MainActivity.kt
+++ app/src/main/java/com/example/schedulog/MainActivity.kt
@@ -10,6 +10,7 @@ import android.provider.MediaStore
 import android.util.Base64
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -19,8 +20,6 @@ import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import timber.log.Timber
 import java.io.ByteArrayOutputStream
-import java.io.File
-
 
 class MainActivity : AppCompatActivity() {
     /*variables for selecting images from gallery*/
@@ -33,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         Timber.i("onCreate Called")
 
-        //setContentView(R.layout.activity_events)
+        //setContentView(R.layout.activity_events) for scrollable view
 
         setContentView(R.layout.activity_main)
 
@@ -42,7 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         writeBtn.setOnClickListener {
             val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-            startActivityForResult(gallery, pickImage)
+            startActivityForResult(gallery, pickImage)      // disregard java deprecation
         }
 
         readBtn.setOnClickListener() {
@@ -85,14 +84,13 @@ class MainActivity : AppCompatActivity() {
 
 
     /** Functions and methods **/
-
+    // overriding activity result solely for uploading images as strings to the DB
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == pickImage) {
+            Timber.i("Picked image and result OK")
             imageUri = data?.data
             imagePath = imageUri?.let { getRealPathFromURI(it) }
-            Timber.i("imagePath: %s", imagePath)
-
             testWrite(imagePath)
         }
     }
@@ -101,22 +99,15 @@ class MainActivity : AppCompatActivity() {
         // Write a message to the database
         val database = Firebase.database
         val myRef = database.getReference("images")
-        // get image from phone
-
-        // convert image to bitmap to bytearray for storing into DB
+        // convert image string 'selectedImagePath' to bitmap to bytearray for storing into DB
         val bm = BitmapFactory.decodeFile(selectedImagePath)
         val baos = ByteArrayOutputStream()
         bm.compress(Bitmap.CompressFormat.JPEG, 100, baos) // bm is the bitmap object
-
         val b = baos.toByteArray()
-
         val encodedImage: String = Base64.encodeToString(b, Base64.DEFAULT)
-
-        val value = encodedImage
-
         // put into DB and show in logcat
-        myRef.setValue(value)
-        Timber.i("%s | Writing | %s", myRef, value)
+        myRef.setValue(encodedImage)
+        Timber.i("%s | Writing | %s", myRef, encodedImage)
     }
 
     private fun getRealPathFromURI(contentURI: Uri): String? {
@@ -132,7 +123,7 @@ class MainActivity : AppCompatActivity() {
 
     fun testRead(){
         val database = Firebase.database
-        val myRef = database.getReference("Justin")
+        val myRef = database.getReference("images")
         // Read from the database
         myRef.addValueEventListener(object: ValueEventListener {
 
@@ -141,6 +132,16 @@ class MainActivity : AppCompatActivity() {
                 // whenever data at this location is updated.
                 val value = snapshot.getValue<String>()
                 Timber.i("%s | Reading | %s" , myRef, value)
+                // Decoding image string from the DB and sending it to the activity
+                try{
+                    val imageBytes: ByteArray = Base64.decode(value, Base64.DEFAULT)
+                    val decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+                    val iView = findViewById<ImageView>(R.id.imageView)
+                    iView.setImageBitmap(decodedImage)
+                }
+                catch (e: Error){
+                    Timber.i("Error when uploading image")
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
