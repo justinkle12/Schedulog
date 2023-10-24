9c683117a78c00f2efb0852fab1530d15feac370
Famasary <32050415+Famasary@users.noreply.github.com>
2023-10-23 21:10:58 -0700

    added list_item_event_history

--- app/src/main/java/com/example/schedulog/EventHistoryAdapter.kt
+++ app/src/main/java/com/example/schedulog/EventHistoryAdapter.kt
@@ -3,7 +3,7 @@ package com.example.schedulog
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.schedulog.databinding.ListItemEventBinding
+import com.example.schedulog.databinding.ListItemEventHistoryBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
@@ -12,7 +12,7 @@ class EventHistoryAdapter(
     private val onAttendEventClick: (EventHistoryFragment.Event) -> Unit
 ) : RecyclerView.Adapter<EventHistoryAdapter.EventViewHolder>() {
 
-    class EventViewHolder(private val binding: ListItemEventBinding) :
+    class EventViewHolder(private val binding: ListItemEventHistoryBinding) :
         RecyclerView.ViewHolder(binding.root) {
         private fun onAttendEventClick(event: EventHistoryFragment.Event) {
             val currentUserUid = FirebaseAuth.getInstance().currentUser?.uid
@@ -27,19 +27,16 @@ class EventHistoryAdapter(
         }
 
         fun bind(event: EventHistoryFragment.Event) {
-            binding.eventTitle.text = event.title
-            binding.eventDescription.text = event.description
-            binding.eventDate.text = event.date
-            binding.eventStartEndTime.text = event.startEndTime
+            binding.eventHistoryTitle.text = event.title
+            binding.eventHistoryDescription.text = event.description
+            binding.eventHistoryDate.text = event.date
+            binding.eventHistoryStartEndTime.text = event.startEndTime
             binding.eventUserID.text = event.user
-            binding.attendEventButton.setOnClickListener {
-                onAttendEventClick(event)
-            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
-        val binding = ListItemEventBinding.inflate(
+        val binding = ListItemEventHistoryBinding.inflate(
             LayoutInflater.from(parent.context), parent, false
         )
         return EventViewHolder(binding)
